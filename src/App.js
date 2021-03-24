b28644356e7df8a3564748667667e181854af7f1
Ganesh Moota <ganeshnm2000@gmail.com>
2021-03-24 15:36:20 +0530

    fetch and update state from react components using mapStateToProps and mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import CakeContainer from './components/CakeContainer';
 import store from './redux/store';
 
 function App() {
     return (
         <Provider store={store}>
             <div>
-                React App
-                {console.log(store.getState())}
+                <CakeContainer/>
             </div>
         </Provider>
     )
