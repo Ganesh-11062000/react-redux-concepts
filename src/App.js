fee58b8ce3066fcd114d915f9c927e064c9dc059
Ganesh Moota <ganeshnm2000@gmail.com>
2021-03-24 15:53:02 +0530

    replacing connect HOC with useSelector and useDispatch Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
 import store from './redux/store';
 
 function App() {
     return (
         <Provider store={store}>
             <div>
+                <HooksCakeContainer/>
                 <CakeContainer/>
             </div>
         </Provider>
